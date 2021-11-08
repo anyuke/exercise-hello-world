5412042407a9e995f91f059697f652116eb10d60
anyuke <1249836965@qq.com>
2021-11-08 11:48:17 +0800

    test

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@nestjs/common';
 export class AppService {
   getHello(): string {
     console.log('-----start-----');
-    return 'Hello World!';
+    return 'Hello World 1!';
   }
 }
